9d1c8885c4dd132459dddc865461fb2cf4de6b60
Noah <151690721+pedro-noah@users.noreply.github.com>
2024-03-05 21:11:32 -0300

    Primeira importação do commit inicial
    Adição do menu, inicialização, projeto biscoito da sorte e projeto de rolagem de dados

---
+++ main50.py
@@ -0,0 +1,60 @@
+import random
+
+#FUNÇÕES MENU
+def titulo_projeto():
+    print('''
+        
+█▀ █▀█   █▀█ █▀█ █▀█ ░░█ █▀▀ ▀█▀ █▀█ █▀
+▄█ █▄█   █▀▀ █▀▄ █▄█ █▄█ ██▄ ░█░ █▄█ ▄█
+    ''')
+
+def menu_principal():
+    print('''
+        1. Biscoito da Sorte
+        2. Rolagem de Dados
+        ...
+    ''')
+    opcao_menu_principal = int(input('Digite qual projeto deseja extrair: '))
+    print()
+    if opcao_menu_principal == 1:
+        pjt_biscoito_sorte()
+    elif opcao_menu_principal == 2:
+        pjt_rolagem_dado()
+    elif opcao_menu_principal == 3:
+        pjt_pedra_papel_tesoura()
+
+
+
+#FUNÇÕES DO PROJETO
+def pjt_biscoito_sorte():
+    dic_biscoito = [{'num': 1, 'frase':'O sucesso virá para aqueles que trabalham duro e persistem.', 'numero':'18 45 65'}, 
+                {'num': 2, 'frase':'A vida é uma jornada - aproveite o caminho tanto quanto o destino.', 'numero':'88 25 60'},
+                {'num': 3, 'frase':'Seja gentil sempre, pois você nunca sabe o impacto que suas palavras podem ter.', 'numero':'32 65 23'},
+                {'num': 4, 'frase':'Grandes coisas muitas vezes têm começos humildes.', 'numero':'84 45 09'},
+                {'num': 5, 'frase':'Acredite em si mesmo e outros também acreditarão em você.', 'numero':'75 76 45'}]
+    num_biscoito = random.randrange(1, 5)
+    for biscoito in dic_biscoito:
+        if num_biscoito == biscoito['num']:
+            print(biscoito['frase'])
+            print(biscoito['numero'])
+
+def pjt_rolagem_dado():
+    print('Jogando os dados...')
+    dado1 = random.randrange(1, 6)
+    dado2 = random.randrange(1, 6)
+    print(f'Cairam os numeros {dado1} e {dado2}')
+    print(f'O valor final foi de:  {dado2+dado1}')
+
+def pjt_pedra_papel_tesoura():
+    print('...')
+
+
+
+    
+#INICIALIZADOR
+def main():
+    titulo_projeto()
+    menu_principal()
+
+if __name__ == "__main__":
+    main()
